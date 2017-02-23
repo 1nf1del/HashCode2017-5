aa62d457d69fec4f5d25f46727987aed2e9d20a4
Julien Déoux <julien.deoux@telecomnancy.net>
2017-02-23 21:50:54 +0100

    Friendlier main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 from model import Model
 from firstAlgo import firstAlgo
+import sys
+import os
 
-fileDesc = open('../inputs/kittens.in')
-model = Model(fileDesc)
+if len(sys.argv) == 1:
+    print("Usage: python2 main.py [FILES]")
 
-print(firstAlgo(model))
+for filePath in sys.argv[1:]:
+    inputFile = open(filePath)
+    fileName,fileExtension = os.path.splitext(os.path.basename(filePath))
+    outputFile = open(fileName + ".txt", "w")
+    
+    print("Importing file " + fileName + fileExtension)
+    model = Model(inputFile)
+    print("Model imported")
+    inputFile.close()
+
+    outputFile.write(firstAlgo(model))
+    print("Output file " + fileName + ".txt generated")
+    outputFile.close()
