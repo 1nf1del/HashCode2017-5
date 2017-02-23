cc0c81f86712f471b8e75108f8428f668bae7805
DECELLE Remi <remi.decelle@telecomnancy.eu>
2017-02-23 21:50:11 +0100

    sort fonctions faites

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,5 +1,5 @@
 class Endpoint():
-    
+
     def __init__(self, ld, caches):
         # self.ld      = Latence du Data Center vers l'Endpoint
         # caches = Listes des cacheServers connectes a l'Endpoint | format : Map < idCacheServer, Latence >
@@ -8,8 +8,7 @@ class Endpoint():
         self.caches = caches
 
     def sortCaches(self):
-        pass
+        caches.sort(key=lambda tup: tup[1])
 
     def chooseCache(self, video):
         pass
-
